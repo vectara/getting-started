6436d93d135102d63fc733202488858403776a43
Aamir Butt <aamir@vectara.com>
2023-12-01 22:08:02 +0400

    Addressed some code review comments.

--- feature-examples/python/create_apikey.py
+++ feature-examples/python/create_apikey.py
@@ -1,7 +1,6 @@
 """Example of using the Vectara REST API to create an API Key."""
 
 import argparse
-import json
 import logging
 import requests
 import sys
@@ -15,7 +14,7 @@ def create_apikey(
     auth_url: str,
     app_client_id: str,
     app_client_secret: str,
-):
+) -> (any, bool):
     """Creates an API key.
 
     Args:
@@ -27,12 +26,11 @@ def create_apikey(
 
     Returns:
         (response, True) in case of success and returns (error, False) in case of failure.
-
     """
     jwt_token = vectara_util.get_jwt_token(auth_url, app_client_id, app_client_secret)
     if not jwt_token:
         logging.error("Failed to get JWT token.")
-        return None, False
+        return "Failed to get JWT token.", False
 
     post_headers = {
         "customer-id": f"{customer_id}",
@@ -52,7 +50,7 @@ def create_apikey(
 
     response = requests.post(
         f"https://api.vectara.io/v1/create-api-key",
-        data=json.dumps(request),
+        json=request,
         verify=True,
         headers=post_headers,
     )
@@ -72,29 +70,31 @@ def create_apikey(
             logging.error("CreateApiKey failed with response %s", message["response"])
             return message["response"], False
         status = message["response"][0]["status"]
-        if status["code"] != "OK":
+
+        if status["code"] == "OK":
+            return message["response"][0]["keyId"], True
+        else:
             logging.error("CreateApiKey failed with status %s", status)
             return status, False
 
-        return message["response"][0]["keyId"], True
     return message, False
 
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO
-    )
-
+def main():
     parser = argparse.ArgumentParser(
         description="Vectara example to create an API key."
     )
 
     parser.add_argument(
-        "--customer-id", type=int, help="Unique customer ID in Vectara platform."
+        "--customer-id",
+        type=int,
+        required=True,
+        help="Unique customer ID in Vectara platform.",
     )
     parser.add_argument(
         "--corpus-id",
         type=int,
+        required=True,
         help="Corpus ID to which API key will be created.",
     )
     parser.add_argument(
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--app-client-secret", required=True)
     parser.add_argument(
-        "--auth-url", required=True, help="The cognito auth url for this customer."
+        "--auth-url", required=True, help="The auth url for this customer."
     )
 
     args = parser.parse_args()
@@ -120,3 +120,10 @@ if __name__ == "__main__":
         logging.info("CreateApiKey response: %s", response)
         if not status:
             sys.exit(1)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO
+    )
+    main()
